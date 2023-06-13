e7f93712d04f48666cbc275881b95c0a920e4191
JHS <117875637+zzoall@users.noreply.github.com>
2023-06-13 23:59:41 +0900

    update function savedToDos, saveToDos

--- js/todo.js
+++ js/todo.js
@@ -3,6 +3,16 @@ const toDoForm = document.querySelector("#todo-form");
 const toDoInput = toDoForm.querySelector("input")
 const toDoList= document.querySelector("#todo-list");
 
+const TODOS_KEY = "todos"
+
+// 값이 바뀌기 때문에 let을 사용해야함.
+let toDos = [];
+
+function saveToDos() {
+    // 함수 호출될때 이미 newTodo는 array에 들어있음 (handletodosubmit에서 확인 가능)
+    // JSON.stringify()는 js의 object와 array를 string으로 변환시킴
+    localStorage.setItem("todos", JSON.stringify(toDos))
+}
 
 function deleteTodo(event) {
     // event.target은 클릭된 html의 element를 알려줌.
@@ -29,8 +39,24 @@ function handleToDoSubmit(event) {
     event.preventDefault();
     const newTodo = toDoInput.value; // newTodo라는 변수에 input값 복사하고
     toDoInput.value = ""; // enter 누르면 새로고침되어 입력한 글은 빈칸으로 채움.
+    // newTodo를 array에 저장하고 화면에 띄우고나서 로컬스토리지에 저장(saving 해줘야함.)
+    toDos.push(newTodo);
     paintToDo(newTodo);
+    saveToDos();
 }
 
 toDoForm.addEventListener("submit", handleToDoSubmit);
+
+
+// 단순한 string을 array로
+const savedToDos = localStorage.getItem(TODOS_KEY);
+
+if (savedToDos !== null) {
+    const parsedToDos = JSON.parse(savedToDos);
+    // toDos가 항상 빈 array []; 다 보니 localstorage에 새로운 것만 저장이 되어서 이전에 있던 toDos를 복원할 수 있도록 함.(더 이상 빈값이 아님).
+    toDos = parsedToDos;
+    //  js는 텍스트(array 각각)를 paintToDo에게 전달해준다.
+    // paintToDo("a"),b,c,d,e.... 이런식으로!
+    parsedToDos.forEach(paintToDo);
+}
 
