6f321a0470f18b65291b46e974fb1000e0df5c38
JHS <117875637+zzoall@users.noreply.github.com>
2023-06-16 14:36:04 +0900

    update weather.js
    weather API

--- js/weather.js
+++ js/weather.js
@@ -4,9 +4,16 @@ function onGeoOk(position) {
     const lat = position.coords.latitude;
     const lng = position.coords.longitude;
     const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}$units=metric`;
-    // Get the url
-    fetch(url);
-}
+    // Get the url (js is calling the url and the name of city, weather.)
+    fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+        const weather = document.querySelector('#weather span:first-child');
+        const city = document.querySelector('#weather span:last-child');
+        city.innerText = data.name;
+        weather.innerText = data.weather[0].main});
+
+}   
 function onGeoError() {
     alert("can't find you.");
 }
